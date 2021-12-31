dbaf2c688918d80395a9ec03f49aba4e56006681
rs <96896020+rs1703@users.noreply.github.com>
2021-12-31 14:14:23 +0700

    feat: scheduled update

--- prefs/library.go
+++ prefs/library.go
@@ -2,25 +2,31 @@ package prefs
 
 import (
 	. "nonbiri/constants"
+	"time"
 
 	"github.com/spf13/viper"
 )
 
 type LibraryPreference struct {
-	Sort  Sort  `json:"sort"`
-	Order Order `json:"order"`
+	Sort            Sort          `json:"sort"`
+	Order           Order         `json:"order"`
+	UpdateFrequency time.Duration `json:"updateFrequency"`
+	LastUpdated     int64         `json:"lastUpdated"`
 }
 
 var Library = &LibraryPreference{
-	Sort:  Sorts.LatestUploadedChapter,
-	Order: Orders.DESC,
+	Sort:            Sorts.LatestUploadedChapter,
+	Order:           Orders.DESC,
+	UpdateFrequency: 2,
 }
 
 func (*LibraryPreference) Update(new *LibraryPreference) {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	*Library = *new
+	if new != nil {
+		*Library = *new
+	}
 	viper.Set("library", Library)
 	viper.WriteConfig()
 }
